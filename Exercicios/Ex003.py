78bc8add50373d936def56622c08770da7c313a6
Pedro Henrique <122459206+Pedrohenrique45@users.noreply.github.com>
2024-04-02 15:50:07 -0300

    update ex003

--- Exercicios/Ex003.py
+++ Exercicios/Ex003.py
@@ -1,2 +1,8 @@
 #Com um determinado número integral n, escreva um programa para gerar um dicionário que contenha (i, i*i) tal que seja um número inteiro entre 1 e n (ambos incluídos). e então o programa deverá imprimir o dicionário. Suponha que a seguinte entrada seja fornecida ao programa: 8 Então, a saída deve ser: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64 }
 
+n=int(input("Digite um número: ")) 
+d=dict()
+for i in range(1,n+1):
+    d[i]=i*i 
+
+print(d)
