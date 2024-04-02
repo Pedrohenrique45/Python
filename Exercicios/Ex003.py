f08232318943263d32acb259f23e169703273d0f
Pedro Henrique <122459206+Pedrohenrique45@users.noreply.github.com>
2024-04-02 14:01:50 -0300

    questão 003

---
+++ Exercicios/Ex003.py
@@ -0,0 +1,2 @@
+#Com um determinado número integral n, escreva um programa para gerar um dicionário que contenha (i, i*i) tal que seja um número inteiro entre 1 e n (ambos incluídos). e então o programa deverá imprimir o dicionário. Suponha que a seguinte entrada seja fornecida ao programa: 8 Então, a saída deve ser: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64 }
+
