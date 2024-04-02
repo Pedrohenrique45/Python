9bca76ff6a74a3e3462a2563a6a8b49dfba3a830
Pedro Henrique <122459206+Pedrohenrique45@users.noreply.github.com>
2024-04-02 13:49:09 -0300

    exercicio 002

--- Exercicios/Ex002.py
+++ Exercicios/Ex002.py
@@ -1,2 +1,8 @@
 #Escreva um programa que possa calcular o fatorial de um determinado número. Os resultados devem ser impressos em sequência separada por vírgula em uma única linha. Suponha que a seguinte entrada seja fornecida ao programa: 8 Então, a saída deveria ser: 40320
+def fatorial(x):
+    if x == 0:
+        return 1
+    return x * fatorial(x -1)
 
+valor = int(input("Qual o fatorial? "))
+print(fatorial(valor))
